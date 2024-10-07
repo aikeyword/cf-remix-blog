ea2f31986c2d37ef7b903d55069a01e58a7284f2
orztv <qi@outlook.cz>
2024-10-08 00:01:54 +0800

    update

--- build.js
+++ build.js
@@ -6,15 +6,7 @@ const wranglerConfig = toml.parse(fs.readFileSync('./wrangler.toml', 'utf-8'));
 
 // 输出当前的 BLOG_SETTINGS
 console.log('Current BLOG_SETTINGS:', process.env.Build_BLOG_SETTINGS);
-console.log('Current BLOG_SETTINGS:', process.env.blog_test);
-console.log('Current BLOG_SETTINGS:', Cloudflare.env.Build_BLOG_SETTINGS);
-console.log('Current BLOG_SETTINGS:', Cloudflare.env.blog_test);
-console.log('Current BLOG_SETTINGS: ${env.Build_BLOG_SETTINGS}');
-console.log('Current BLOG_SETTINGS: ${env.blog_test}');
-console.log('Current BLOG_SETTINGS: ${ process.env.Build_BLOG_SETTINGS }');
-console.log('Current BLOG_SETTINGS: ${ process.env.blog_test }');
-console.log('Current BLOG_SETTINGS: ${ Cloudflare.env.Build_BLOG_SETTINGS }');
-console.log('Current BLOG_SETTINGS: ${ Cloudflare.env.blog_test }');
+
 
 // 如果需要更新 BLOG_SETTINGS，可以在这里进行
 // wranglerConfig.vars.BLOG_SETTINGS = newSettings;
